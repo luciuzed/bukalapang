87c4f8eb6dc5e5bc5dd271c6fbc00c8fefc67d0c
Luciuz <luciuzed@gmail.com>
2026-03-13 10:46:45 +0700

    dashboard ui revamp

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,13 +1,10 @@
 import { Routes, Route } from "react-router-dom"
-import Navbar from "./components/Navbar"
 import LoginPage from "./page/LoginPage"
 import Dashboard from "./page/Dashboard"
 
 function App() {
   return (
-    <div className="container mx-auto px-10">
-      <Navbar />
-
+    <div className="min-h-screen">
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
